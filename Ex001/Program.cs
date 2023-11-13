4ff09137de22cb23815904a2381c7fd80b0fd2f2
Ikul23 <ikul23@icloud.com>
2023-11-13 20:55:37 +0300

    First programm in C#

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(" 12 Hello, World!");
+﻿using System;
+
+class Program {
+  static void Main(string[] args) {
+
+    Console.Write("Введите первое число: ");
+    int a = Int32.Parse(Console.ReadLine());
+
+    Console.Write("Введите второе число: ");    
+    int b = Int32.Parse(Console.ReadLine());
+
+    if (a == b * b) {
+      Console.WriteLine("Да, первое число является квадратом второго");
+    }
+    else {
+      Console.WriteLine("Нет, первое число не является квадратом второго"); 
+    }
+
+  }
+}
