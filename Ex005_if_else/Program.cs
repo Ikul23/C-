2307fce66aed6fd9e66fb3ee936648391ff050d5
Ikul23 <ikul23@icloud.com>
2023-11-14 15:28:24 +0300

    Added new project Ex005_if_else

---
+++ Ex005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "igor")
+{
+    Console.WriteLine("Ура,это же Igor");
+}
+else
+{
+    Console.Write("Привет!, ");
+    Console.WriteLine(username);
+}
