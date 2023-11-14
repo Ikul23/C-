27e0e7e5288c14c91d2857d4e4f4e5f7905d6ab1
Ikul23 <ikul23@icloud.com>
2023-11-14 23:19:30 +0300

    Added new project Ex007_dog

---
+++ Ex007_dog/Program.cs
@@ -0,0 +1,20 @@
+﻿int distance = 10000;
+int FirstFriendSpeed = 1;
+int SecondFriendSpeed = 2;
+int DogSpeed = 5;
+int count = 0;
+int friend = 2;
+
+while(distance < 10)
+{
+    if(friend 1);
+    {
+    time = distance / (FirstFriendSpeed + DogSpeed);
+    }
+    if(friend 2);
+    {
+    time = distance / (SecondFriendSpeed + DogSpeed);          
+    }
+    distance = distance - (FirstFriendSpeed + SecondFriendSpeed) * time;
+        count++;
+}
