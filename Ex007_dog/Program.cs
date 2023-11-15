f8baa230b3a9f50d64347ebff51fc87a52dd028c
Ikul23 <ikul23@icloud.com>
2023-11-15 19:47:24 +0300

    First code added

--- Ex007_dog/Program.cs
+++ Ex007_dog/Program.cs
@@ -1,20 +1,35 @@
-﻿int distance = 10000;
-int FirstFriendSpeed = 1;
-int SecondFriendSpeed = 2;
-int DogSpeed = 5;
-int count = 0;
-int friend = 2;
+﻿using System;
 
-while(distance < 10)
+class Program
 {
-    if(friend 1);
+    static void Main()
     {
-    time = distance / (FirstFriendSpeed + DogSpeed);
+        // Исходные данные
+        int distance = 10000;  // Начальное расстояние между друзьями
+        int firstFriendSpeed = 1;  // Скорость первого друга
+        int secondFriendSpeed = 2;  // Скорость второго друга
+        int dogSpeed = 5;  // Скорость собаки
+        int count = 0;  // Счетчик пройденных интервалов времени
+
+        // Цикл продолжается, пока расстояние между друзьями больше 10
+        while (distance > 10)
+        {
+            // Рассчитываем время, которое нужно первому другу и второму другу
+            double timeToReachFirstFriend = (double)distance / (firstFriendSpeed + dogSpeed);
+            double timeToReachSecondFriend = (double)distance / (secondFriendSpeed + dogSpeed);
+
+            // Используем время второго друга (можно также использовать время первого друга)
+            double timeToReach = timeToReachSecondFriend;
+
+            // Округляем время вверх и добавляем его к общему времени
+            int roundedTimeToReach = (int)Math.Ceiling(timeToReach);
+            count += roundedTimeToReach;
+
+            // Обновляем расстояние в зависимости от времени, которое затратил второй друг и собака
+            distance -= (secondFriendSpeed + dogSpeed) * roundedTimeToReach;
+        }
+
+        // Выводим результат - количество пройденных интервалов времени
+        Console.WriteLine(count);
     }
-    if(friend 2);
-    {
-    time = distance / (SecondFriendSpeed + DogSpeed);          
-    }
-    distance = distance - (FirstFriendSpeed + SecondFriendSpeed) * time;
-        count++;
 }
